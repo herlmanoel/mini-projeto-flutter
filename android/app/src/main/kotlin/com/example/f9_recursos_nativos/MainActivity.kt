8eb1665ae9ae8232b9f251cda5ddbcd9baccef49
herlmanoel <herlmanoelfernandes@gmail.com>
2022-07-07 20:34:24 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/f9_recursos_nativos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f9_recursos_nativos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/place_form_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+import 'package:f9_recursos_nativos/components/image_input.dart';
+import 'package:f9_recursos_nativos/components/location_input.dart';
+import 'package:f9_recursos_nativos/provider/great_places.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  @override
+  _PlaceFormScreenState createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  //deve receber a imagem
+  File? _pickedImage;
+
+  void _selectImage(File? pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  //
+
+  void _submitForm() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    print('tem os dados');
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage!);
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Novo Lugar'),
+      ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    ImageInput(_selectImage),
+                    const SizedBox(height: 10),
+                    LocationInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              primary: Theme.of(context).colorScheme.secondary,
+              onPrimary: Colors.black,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            onPressed: _submitForm,
+          ),
+        ],
+      ),
+    );
+  }
+}
